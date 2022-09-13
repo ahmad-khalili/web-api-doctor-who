9b2c6a03641110abf43ccd1b0915e9106ce6a415
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-13 17:23:28 +0300

    Create an AutoMapper profile between Doctor & DoctorInfo

---
+++ DoctorWho.Web/Profiles/DoctorProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DoctorWho.Db.Entities;
+using DoctorWho.Db.Models;
+
+namespace DoctorWho.Web.Profiles;
+
+public class DoctorProfile : Profile
+{
+    public DoctorProfile()
+    {
+        CreateMap<Doctor, DoctorInfo>();
+    }
+}
