4ba385acd92e3346ba0c74802acf8c4e94d2898a
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-13 16:54:25 +0300

    Inject DoctorRepository & DbContext services

--- DoctorWho.Web/Program.cs
+++ DoctorWho.Web/Program.cs
@@ -1,3 +1,7 @@
+using DoctorWho.Db;
+using DoctorWho.Db.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DoctorWhoCoreDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:DoctorWhoDb"]);
+});
+
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
 
 var app = builder.Build();
 
