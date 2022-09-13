cb132822b8f2f3d117503ce1557b4303e32416bd
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-13 17:23:41 +0300

    Inject AutoMapper into services

--- DoctorWho.Web/Program.cs
+++ DoctorWho.Web/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDbContext<DoctorWhoCoreDbContext>(options =>
 
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
