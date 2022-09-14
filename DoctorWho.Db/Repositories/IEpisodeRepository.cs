cb16031930d1fc27134a8bf630cfac54a5762d3c
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-14 19:35:33 +0300

    Add required methods to the EpisodeRepository interface

--- DoctorWho.Db/Repositories/IEpisodeRepository.cs
+++ DoctorWho.Db/Repositories/IEpisodeRepository.cs
@@ -6,4 +6,9 @@ namespace DoctorWho.Db.Repositories;
 public interface IEpisodeRepository
 {
     Task<(IEnumerable<Episode>, PaginationMetadata)> GetEpisodesAsync(int pageNumber, int pageSize);
+    Task AddEpisodeAsync(Episode episode);
+    Task<bool> AuthorExistsAsync(int authorId);
+    Task<bool> DoctorExistsAsync(int doctorId);
+    Task<Episode?> GetEpisodeAsync(int episodeId);
+    Task<bool> SaveChangesAsync();
 }
