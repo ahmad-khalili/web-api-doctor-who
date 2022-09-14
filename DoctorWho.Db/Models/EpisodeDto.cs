93f73c0e849f284a7485d56f913249a18101028d
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-14 18:52:08 +0300

    Create an Episode DTO

---
+++ DoctorWho.Db/Models/EpisodeDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DoctorWho.Db.Models;
+
+public class EpisodeDto
+{
+    public int EpisodeId { get; set; }
+    public int SeriesNumber { get; set; }
+    public int EpisodeNumber { get; set; }
+    public string? EpisodeType { get; set; }
+    public string? Title { get; set; }
+    public DateTime EpisodeDate { get; set; }
+    public int AuthorId { get; set; }
+    public int DoctorId { get; set; }
+    public string? Notes { get; set; }
+}
