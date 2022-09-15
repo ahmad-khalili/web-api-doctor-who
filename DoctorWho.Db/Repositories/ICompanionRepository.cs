50e227f810e4d9d4610effa4005e39645c68a7e9
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-15 15:55:43 +0300

    Create interface for CompanionRepository

---
+++ DoctorWho.Db/Repositories/ICompanionRepository.cs
@@ -0,0 +1,10 @@
+﻿using DoctorWho.Db.Entities;
+
+namespace DoctorWho.Db.Repositories;
+
+public interface ICompanionRepository
+{
+    Task AddCompanionToEpisodeAsync(int episodeId, Companion companion);
+    Task<bool> EpisodeExistsAsync(int episodeId);
+    Task<bool> SaveChangesAsync();
+}
