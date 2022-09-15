af7d78ac830ae2e00fe27082be80edd75bf1dbdc
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-15 16:13:18 +0300

    Create Update DTO & map its value using AutoMapper

---
+++ DoctorWho.Web/Profiles/AuthorProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DoctorWho.Db.Entities;
+using DoctorWho.Db.Models;
+
+namespace DoctorWho.Web.Profiles;
+
+public class AuthorProfile : Profile
+{
+    public AuthorProfile()
+    {
+        CreateMap<AuthorForUpdateDto, Author>();
+    }
+}
