d77671f19e3a7f489c9428738166261c8bb494df
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-09-15 15:54:59 +0300

    Create required DTOs & map their values

---
+++ DoctorWho.Db/Models/CompanionForCreationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace DoctorWho.Db.Models;
+
+public class CompanionForCreationDto
+{
+    public string CompanionName { get; set; }
+    public string WhoPlayed { get; set; }
+}
---
+++ DoctorWho.Web/Profiles/CompanionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DoctorWho.Db.Entities;
+using DoctorWho.Db.Models;
+
+namespace DoctorWho.Web.Profiles;
+
+public class CompanionProfile : Profile
+{
+    public CompanionProfile()
+    {
+        CreateMap<Companion, CompanionDto>();
+        CreateMap<CompanionForCreationDto, Companion>();
+    }
+}
